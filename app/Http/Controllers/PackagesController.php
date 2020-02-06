2f8564f16da4bd6e7909d5c57b4b075d679b5113
b.slaveykov <b.slaveykov@radioactive.bg>
2020-02-06 17:28:06 +0200

    update

--- app/Http/Controllers/PackagesController.php
+++ app/Http/Controllers/PackagesController.php
@@ -6,6 +6,26 @@ use Illuminate\Http\Request;
 
 class PackagesController extends Controller
 {
+    private $whmcs_url = false;
+    private $repositories = [];
+
+    public function __construct() {
+
+        $satis = file_get_contents('../satis.json');
+        $satis = json_decode($satis, true);
+
+        if ($satis) {
+            $this->whmcs_url = $satis['whmcs_url'];
+            if (isset($satis['repositories'])) {
+                foreach ($satis['repositories'] as $repository) {
+                    $repositoryUrl = $this->_clearRepositoryUrl($repository['url']);
+                    $this->repositories[$repositoryUrl] = $repository;
+                }
+            }
+        }
+
+    }
+
     public function index() {
 
         $packages = $this->_getCompiledPackageJson();
@@ -19,14 +39,16 @@ class PackagesController extends Controller
     private function _getCompiledPackageJson()
     {
         $packages = [];
-        $compiledPackages = $this->_jsonDecodeFile( 'original-packages.json');
+        $compiledPackages = $this->_jsonDecodeFile('original-packages.json');
         if ($compiledPackages) {
             foreach ($compiledPackages as $compiledPackage) {
                 if (is_array($compiledPackage)) {
                     foreach ($compiledPackage as $package=>$packageSha) {
                         $getPackages = $this->_jsonDecodeFile($package);
                         if ($getPackages['packages']) {
-                            $packages = array_merge($packages, $getPackages['packages']);
+                            foreach ($getPackages['packages'] as $packageName=>$packageVersions) {
+                                $packages[$packageName] = $this->_prepareVersions($packageVersions);
+                            }
                         }
                     }
                 }
@@ -34,6 +56,46 @@ class PackagesController extends Controller
         }
 
         return $packages;
+    }
+
+    private function _prepareVersions($versions) {
+
+        $prepareVersions = [];
+        foreach ($versions as $version) {
+            $prepareVersions[] = $this->_preparePackage($version);
+        }
+
+        return $prepareVersions;
+    }
+
+    private function _preparePackage($package) {
+
+        $packageUrl = $this->_clearRepositoryUrl($package['source']['url']);
+        if (isset($this->repositories[$packageUrl])) {
+            $repositorySettings = $this->repositories[$packageUrl];
+            if (isset($repositorySettings['whmcs_license_ids']) && !empty($repositorySettings['whmcs_license_ids'])) {
+                $package['dist'] = [
+                    "type" => "license_key",
+                    "url" => "",
+                    "reference" => "license_key",
+                    "shasum" => "license_key"
+                ];
+            }
+        }
+
+        return $package;
+    }
+
+    private function _clearRepositoryUrl($repositoryUrl) {
+
+        $repositoryUrl = str_replace('https://', false, $repositoryUrl);
+        $repositoryUrl = str_replace('http://', false, $repositoryUrl);
+        $repositoryUrl = str_replace('http://wwww.', false, $repositoryUrl);
+        $repositoryUrl = str_replace('https://wwww.', false, $repositoryUrl);
+        $repositoryUrl = str_replace(':', '/', $repositoryUrl);
+        $repositoryUrl = str_replace('git@', '', $repositoryUrl);
+
+        return $repositoryUrl;
     }
 
     private function _jsonDecodeFile($file) {
