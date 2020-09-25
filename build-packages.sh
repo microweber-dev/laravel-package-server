fc87ed47db60c463a29f570a722f822e4984f6eb
bobimicroweber <bobi@microweber.com>
2020-09-25 14:07:48 +0300

    update

--- build-packages.sh
+++ build-packages.sh
@@ -1,6 +1,4 @@
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-php -d memory_limit=-1 $dir/artisan package-manager:change-satis-schema
-php -d memory_limit=-1 $dir/vendor/composer/satis/bin/satis build $dir/satis.json public --stats -n
-mv $dir/public/packages.json $dir/public/original-packages.json
-php -d memory_limit=-1 $dir/artisan package-manager:build
+php -d memory_limit=-1 artisan package-manager:change-satis-schema
+php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./satis.json public --stats -n
+mv public/packages.json public/original-packages.json
+php -d memory_limit=-1 artisan package-manager:build
