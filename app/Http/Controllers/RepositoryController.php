f0a12e7bd11b7a9a4c19c71331f8f77501057b02
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-26 17:06:21 +0300

    update

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -68,7 +68,7 @@ class RepositoryController extends Controller
         }
 
         $whmcsProductsTypes = [];
-        
+
         try {
             $getWhmcsProducts = \Whmcs::GetProducts();
         } catch (\Exception $e) {
