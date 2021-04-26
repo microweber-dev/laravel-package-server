de9bf30b73d4305c2d077b1ba726cb1742b271dd
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-26 16:36:38 +0300

    update

--- build-packages-plesk.sh
+++ build-packages-plesk.sh
@@ -2,3 +2,4 @@ php -d memory_limit=-1 artisan package-manager:change-satis-schema
 php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./satis.json public --stats -n
 mv public/packages.json public/original-packages.json
 php -d memory_limit=-1 artisan package-manager:build
+
