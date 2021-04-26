fbfcb04d9434cbe153a09cefe7129928068d59f0
Bozhidar Slaveykov <bobi@microweber.com>
2021-04-26 11:12:02 +0300

    update

--- build-packages.sh
+++ build-packages.sh
@@ -2,3 +2,4 @@ php -d memory_limit=-1 artisan package-manager:change-satis-schema </dev/null >p
 php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./satis.json public --stats -n </dev/null >public/build-packages-output.log 2>public/build-packages-output.error &
 mv public/packages.json public/original-packages.json </dev/null >public/build-packages-output.log 2>public/build-packages-output.error &
 php -d memory_limit=-1 artisan package-manager:build </dev/null >public/build-packages-output.log 2>public/build-packages-output.error &
+
