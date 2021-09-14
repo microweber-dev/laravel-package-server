b9c045334f21d13bc34a082c3f21f8d912dc7634
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-09-14 17:15:55 +0300

    update

--- build-packages.sh
+++ build-packages.sh
@@ -25,4 +25,3 @@ php -d memory_limit=-1 artisan package-manager:change-satis-schema --env $env </
 php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./config/$env/satis.json public/$env --stats -n </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
 mv public/$env/packages.json public/$env/original-packages.json </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
 php -d memory_limit=-1 artisan package-manager:build --env $env </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
-
