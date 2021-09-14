1f015dea797d58f1edf397512ef46f6bbc2ff8f7
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-09-14 17:14:27 +0300

    update

--- build-packages.sh
+++ build-packages.sh
@@ -22,7 +22,7 @@ if ! [ -d "public/$env/include" ]; then
 fi
 
 php -d memory_limit=-1 artisan package-manager:change-satis-schema --env $env </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
-php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./satis.json public/$env --stats -n </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
+php -d memory_limit=-1 vendor/composer/satis/bin/satis build ./config/$env/satis.json public/$env --stats -n </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
 mv public/$env/packages.json public/$env/original-packages.json </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
 php -d memory_limit=-1 artisan package-manager:build --env $env </dev/null >public/$env/build-packages-output.log 2>public/$env/build-packages-output.error &
 
