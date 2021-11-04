e20e41be615934123876dc92c04a796163a6d8ad
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-11-04 14:34:41 +0200

    Create build-all-envs.sh

---
+++ build-all-envs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CURRENT_FOLDER=$('pwd')
+CONFIGS="config/*/"
+for FOLDER in $CONFIGS
+do
+  if [ -d "$FOLDER" ]
+  then
+
+    ENV_NAME=$(cut -d '/' -f 2 <<< $FOLDER)
+
+    ./build-packages-without-log.sh -e $ENV_NAME
+
+  else
+    echo "Warning: Some problem with \"$FOLDER\""
+  fi
+done
