0c9bcb1953c52f7bd7a59dff590e1af29e384ea4
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-15 16:48:56 +0200

    clean laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Helpers;
 use Illuminate\Support\ServiceProvider;
-use PHPUnit\TextUI\Help;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,24 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $envConfigDir = Helpers::getEnvConfigDir();
-
-        $packageServerConfig = @include($envConfigDir .DIRECTORY_SEPARATOR. 'package-manager.php');
-        if (isset($packageServerConfig['installed']) && $packageServerConfig['installed'] == 1) {
-            $envConfigDirScan = scandir($envConfigDir);
-            foreach ($envConfigDirScan as $envConfigFile) {
-
-                if ($envConfigFile === '.' || $envConfigFile === '..') {
-                    continue;
-                }
-
-                $configName = pathinfo($envConfigFile, PATHINFO_FILENAME);
-                ob_start();
-                $envArray = include($envConfigDir . DIRECTORY_SEPARATOR . $envConfigFile);
-                ob_clean();
-                \Config::set($configName, $envArray);
-            }
-        }
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,54 +13,6 @@
 |
 */
 
-Auth::routes();
-
-use Laravel\Socialite\Facades\Socialite;
-
-Route::get('/auth/{driver}/redirect', function ($driver) {
-    return Socialite::driver($driver)
-        ->scopes(['read_api'])
-        ->redirect();
-})->name('auth.redirect');
-
-Route::get('/auth/{driver}/callback', 'GitSyncController@authCallback')->name('auth.callback');
-Route::post('/git-sync-save', 'GitSyncController@save')->name('gitsync.save');
-
-Route::middleware(['allowed_ips'])->group(function () {
-
-    Route::get('/', function () {
-
-        $file = 'domains/' . \App\Helpers::getEnvName() . '/index.html';
-
-        if (is_file($file)) {
-            echo file_get_contents($file);
-
-        } else {
-            return view('welcome');
-        }
-
-    });
-
-    Route::get('packages.json', 'PackagesController@index')->name('packages-json');
-    Route::get('packages.json/test', 'PackagesController@test');
-    Route::get('home', 'HomeController@index')->name('home');
-
-    Route::get('add-repo', 'RepositoryController@edit')->name('add-repo');
-    Route::post('add-repo', 'RepositoryController@save');
-
-    Route::get('edit-repo', 'RepositoryController@edit')->name('edit-repo');
-    Route::post('edit-repo', 'RepositoryController@save');
-
-    Route::get('build-repo', 'RepositoryController@build')->name('build-repo');
-    Route::get('build-repo-run', 'RepositoryController@buildRun')->name('build-repo-run');
-
-    Route::get('delete-repo', 'RepositoryController@delete');
-
-    Route::get('configure', 'ConfigureController@index')->name('configure');
-    Route::post('configure', 'ConfigureController@save')->name('configure-save');
-
-    Route::get('configure-whmcs', 'WhmcsController@index')->name('configure-whmcs');
-    Route::post('configure-whmcs', 'WhmcsController@save')->name('configure-whmcs-save');
-    Route::post('configure-whmcs-connection-status', 'WhmcsController@getConnectionStatus')->name('configure-whmcs-connection-status');
-
+Route::get('/', function () {
+    return view('welcome');
 });
