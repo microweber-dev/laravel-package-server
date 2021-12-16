e849b2c74890e6d24306f7c308c6eda5f8506a04
Bozhidar Slaveykov <bobi@microweber.com>
2021-12-16 14:23:44 +0200

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -29,6 +29,8 @@
                             <svg class="ms-2" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
                             </svg>
+                                @else
+                                My Teams
                                 @endif
                         </x-slot>
 
@@ -51,6 +53,7 @@
                                 </x-jet-dropdown-link>
                             @endcan
 
+                            @if(Auth::user()->allTeams()->count() > 0)
                             <hr class="dropdown-divider">
 
                             <!-- Team Switcher -->
@@ -61,6 +64,7 @@
                             @foreach (Auth::user()->allTeams() as $team)
                                 <x-jet-switchable-team :team="$team" />
                             @endforeach
+                                @endif
                         </x-slot>
                     </x-jet-dropdown>
                 @endif
