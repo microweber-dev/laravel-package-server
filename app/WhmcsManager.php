8a675ec60f81340aadc483f05ea66652a8b93b85
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2022-01-04 15:59:26 +0200

    update

--- app/WhmcsManager.php
+++ app/WhmcsManager.php
@@ -20,7 +20,7 @@ class WhmcsManager
         }
 
         if (isset($this->config['whmcs_url'])) {
-            Config::set('whmcs.apiurl', $this->config['whmcs_url']);
+            Config::set('whmcs.apiurl', $this->config['whmcs_url']); 
         }
 
         if (isset($this->config['whmcs_api_identifier'])) {
