a3819104418252b1371bc80c0004d3f8a47d2e8c
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2022-01-11 16:31:52 +0200

    Update show.blade.php

--- resources/views/livewire/packages/show.blade.php
+++ resources/views/livewire/packages/show.blade.php
@@ -6,9 +6,32 @@
         </h2>
     </x-slot>
 
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="Information" wire:ignore >
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#Information" type="button" role="tab" aria-controls="Information" aria-selected="true">
+                Information
+            </button>
+        </li>
+        <li class="nav-item" role="presentation" wire:ignore >
+            <button class="nav-link" id="Changelog-tab" data-bs-toggle="tab" data-bs-target="#Changelog" type="button" role="tab" aria-controls="Changelog" aria-selected="false">Changelog</button>
+        </li>
+        <li class="nav-item" role="Downloads" wire:ignore>
+            <button class="nav-link" id="Downloads-tab" data-bs-toggle="tab" data-bs-target="#Downloads" type="button" role="tab" aria-controls="Downloads" aria-selected="false">Downloads</button>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="Information" role="tabpanel" aria-labelledby="Information-tab" wire:ignore.self>
+
+
+        </div>
+        <div class="tab-pane fade" id="Changelog" role="tabpanel" aria-labelledby="Changelog-tab" wire:ignore.self>
+
+        </div>
+        <div class="tab-pane fade" id="Downloads" role="tabpanel" aria-labelledby="Downloads-tab" wire:ignore.self>
+
+        </div>
+    </div>
 
 
 
-
-    
 </div>
