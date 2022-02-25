0b84e3d803f9d977a0a3cb531da83925025ce70d
Bozhidar Slaveykov <bobi@microweber.com>
2022-02-25 14:40:01 +0200

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,14 +12,27 @@ class WebhookController extends Controller
     {
         $hookJson = $request->json()->all();
 
+        $repositoryName = false;
+
         // Github webhook
         if (isset($hookJson['repository']['full_name'])) {
-            $findPackage = Package::where('name', $hookJson['repository']['full_name'])->first();
+            $repositoryName = $hookJson['repository']['full_name'];
+        }
+
+        // Gitlab webhook
+        if (isset($hookJson['project']['path_with_namespace'])) {
+            $repositoryName = $hookJson['project']['path_with_namespace'];
+        }
+
+        if ($repositoryName) {
+            $findPackage = Package::where('name', $repositoryName)->first();
             if ($findPackage != null) {
                 dispatch(new ProcessPackageSatis($findPackage->id));
                 return ['success'=>true];
             }
         }
+
+        return abort('404', 'Project not found');
 
     }
 }
