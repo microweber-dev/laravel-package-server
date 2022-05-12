e496f3bce7c155b9edc364b90f715a9125ba8a6b
Bozhidar Slaveykov <bobi@microweber.com>
2022-05-12 18:45:24 +0300

    update

--- app/View/Columns/ScreenshotColumn.php
+++ app/View/Columns/ScreenshotColumn.php
@@ -6,5 +6,5 @@ use Rappasoft\LaravelLivewireTables\Views\Columns\ImageColumn;
 
 class ScreenshotColumn extends ImageColumn
 {
-    protected string $view = 'livewire.tables.includes.columns.image';
+    protected string $view = 'livewire.tables.includes.columns.screenshot';
 }
