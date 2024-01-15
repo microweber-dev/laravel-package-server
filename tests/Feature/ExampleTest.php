81d18334a520da027c488eedb16bfd553a5ac1bf
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-15 11:43:54 +0200

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature__;
+namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
--- tests/Feature/TwoFactorAuthenticationSettingsTest.php
+++ tests/Feature/TwoFactorAuthenticationSettingsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature__;
+namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
